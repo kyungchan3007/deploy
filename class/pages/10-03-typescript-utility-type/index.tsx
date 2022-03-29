6c2250caf5f0558fa261ed9c9007df959495180a
nwd0907 <nwd0907@newbizstart.com>
2022-03-29 13:42:10 +0900

    Update modal

--- class/pages/10-03-typescript-utility-type/index.tsx
+++ class/pages/10-03-typescript-utility-type/index.tsx
@@ -1,43 +1,40 @@
-export default function TypescriptPage(){
-  
-    interface IProfile {
-        name: string
-        age: number
-        school: string
-        hobby?: string
-    }
+export default function TypescriptPage() {
+  // interface IProfile {
+  //     name: string
+  //     age: number
+  //     school: string
+  //     hobby?: string
+  // }
 
-    // 1. Pick 타입
-    type Mytype1 = Pick<IProfile, "name" | "age">
+  // // 1. Pick 타입
+  // type Mytype1 = Pick<IProfile, "name" | "age">
 
-    // 2. Omit 타입
-    type Mytype2 = Omit<IProfile, "school">
+  // // 2. Omit 타입
+  // type Mytype2 = Omit<IProfile, "school">
 
-    // 3. Partial 타입
-    type Mytype3 = Partial<IProfile>
+  // // 3. Partial 타입
+  // type Mytype3 = Partial<IProfile>
 
-    // 4. Required 타입
-    type Mytype4 = Required<IProfile>
+  // // 4. Required 타입
+  // type Mytype4 = Required<IProfile>
 
-    // 5. Record 타입
-    type ZZZ = "aaa" | "qqq" | "rrr" // Union 타입
-    // let apple: ZZZ
-    // apple = "qqq"
-    type Mytype5 = Record<ZZZ, IProfile>
+  // // 5. Record 타입
+  // type ZZZ = "aaa" | "qqq" | "rrr" // Union 타입
+  // // let apple: ZZZ
+  // // apple = "qqq"
+  // type Mytype5 = Record<ZZZ, IProfile>
 
+  // // ===== 추가(선언병합) - (type vs interface) =====
+  // interface IProfile {
+  //     candy: number
+  // }
 
-    // ===== 추가(선언병합) - (type vs interface) =====
-    interface IProfile {
-        candy: number
-    }
+  // let profile: IProfile
+  // profile = {
+  //     candy: 3,
+  //     age: 10,
+  //     hobby: "asdf"
+  // }
 
-    let profile: IProfile
-    profile = {
-        candy: 3,
-        age: 10,
-        hobby: "asdf"
-    }
-
-
-    return <div>타입스크립트 연습하기!!!</div>
+  return <div>타입스크립트 연습하기!!!</div>;
 }
