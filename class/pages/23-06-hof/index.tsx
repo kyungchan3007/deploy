abcd26c1617abacca350d628fe626d62e7c40fa9
nwd0907 <nwd0907@newbizstart.com>
2022-04-13 13:29:18 +0900

    Update hof

--- class/pages/23-06-hof/index.tsx
+++ class/pages/23-06-hof/index.tsx
@@ -1,13 +1,17 @@
 export default function HofPage() {
-  const onClickChild = (el) => () => () => (event) => {
+  const onClickChild = (el) => (event) => {
     el;
   };
 
   return (
     <div>
       <h1>HOF 연습 페이지입니다!!!</h1>
-      {["철수", "영희", "훈이"].map((el) => (
-        <div key={el} onClick={onClickChild(el)()()}>
+      {[
+        { title: "안녕하세요", writer: "철수", contents: "안녕하세요?!" },
+        { title: "영희입니다", writer: "영희", contents: "날씨가 어때요?" },
+        { title: "훈이예요", writer: "훈이", contents: "이렇게 흐린날" },
+      ].map((el) => (
+        <div key={el} onClick={onClickChild(el)}>
           {el}
         </div>
       ))}
