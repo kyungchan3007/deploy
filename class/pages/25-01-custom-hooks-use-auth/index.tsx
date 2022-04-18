605628785fb7c6541a264d2f20555df2b525ad77
nwd0907 <nwd0907@newbizstart.com>
2022-04-18 14:48:45 +0900

    Update custom-hooks

---
+++ class/pages/25-01-custom-hooks-use-auth/index.tsx
@@ -0,0 +1,10 @@
+// import { withAuth } from "../../src/components/commons/hocs/withAuth"
+import { useAuth } from "../../src/components/commons/hooks/useAuth";
+
+function CustomHooksUseAuthPage() {
+  useAuth();
+
+  return <div>철수의 프로필 페이지입니다!!</div>;
+}
+
+export default CustomHooksUseAuthPage;
